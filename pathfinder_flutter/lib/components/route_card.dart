2eba71c2b9c878830f100e6471e32730c0353609
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-09 18:22:00 +0200

    Added IconButton to RouteCard

--- pathfinder_flutter/lib/components/route_card.dart
+++ pathfinder_flutter/lib/components/route_card.dart
@@ -1,3 +1,4 @@
+import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:pathfinder_client/pathfinder_client.dart';
 
@@ -35,24 +36,36 @@ class RouteCard extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text(route.name),
-                  Text(
-                    route.description,
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: 12,
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(route.name),
+                        Text(
+                          route.description,
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                            fontSize: 12,
+                          ),
+                        ),
+                        Text(
+                          'Number of points: ${route.points.length}',
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                            fontSize: 11,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Text(
-                    'Number of points: ${route.points.length}',
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: 11,
-                    ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(FeatherIcons.chevronRight),
                   ),
                 ],
               ),
