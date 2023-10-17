7279f6b013cd2e0659a5f29e6610c99f5255695a
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-10-17 21:59:14 +0200

    Modified bottom sheet modal content based on tapped visit point

--- pathfinder_flutter/lib/components/pathfinder_bottom_sheet_modal.dart
+++ pathfinder_flutter/lib/components/pathfinder_bottom_sheet_modal.dart
@@ -64,7 +64,8 @@ class PathFinderBottomSheetModal extends StatelessWidget {
                         8,
                       ),
                       child: Image.network(
-                        "https://api.mganczarczyk.pl/tairiku/random/streetmoe?seed=${visitPoint.name}",
+                        visitPoint.images.first?.url ??
+                            "https://api.mganczarczyk.pl/tairiku/random/streetmoe?seed=${visitPoint.name}",
                         fit: BoxFit.cover,
                         height: 200,
                         width: MediaQuery.of(context).size.width,
@@ -85,8 +86,8 @@ class PathFinderBottomSheetModal extends StatelessWidget {
                   const SizedBox(
                     height: 16,
                   ),
-                  const Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  Text(
+                    visitPoint.description ?? "No description",
                     textAlign: TextAlign.justify,
                   ),
                   Align(
